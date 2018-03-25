4259ab13a39b314c90009241058a381d811ba9e6
mayorquinmachines <smayorquin@berkeley.edu>
2018-03-24 21:30:40 -0700

    updated kindbot_logs.py

--- kindbot_logs.py
+++ kindbot_logs.py
@@ -6,30 +6,49 @@ import logging
 import time
 import sys
 from datetime import datetime
+import smbus
  
 from logging.handlers import RotatingFileHandler
 
-sys.path.append('/home/pi/DHT11_Python/')
 import RPi.GPIO as GPIO
-import dht11
 
-sys.path.append('/home/pi/tsl2561/')
-from tentacle_pi.TSL2561 import TSL2561
+#sys.path.append('/home/pi/tsl2561/')
+#from tentacle_pi.TSL2561 import TSL2561
 
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-tsl = TSL2561(0x39,"/dev/i2c-1")
-tsl.enable_autogain()
-tsl.set_time(0x00)
+#tsl = TSL2561(0x39,"/dev/i2c-1")
+#tsl.enable_autogain()
+#tsl.set_time(0x00)
 
 def lux_reading():
     return tsl.lux()
 
-instance = dht11.DHT11(pin = 4)
- 
+
+
+#### New Humid + Temp sensor ######
+bus = smbus.SMBus(1)
+
+def get_humid():
+    bus.write_byte(0x40, 0xF5)
+    time.sleep(0.3)
+    data0 = bus.read_byte(0x40)
+    data1 = bus.read_byte(0x40)
+    humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
+    return humidity
+
+def get_temp():
+    bus.write_byte(0x40, 0xF3)
+    time.sleep(0.3)
+    data0 = bus.read_byte(0x40)
+    data1 = bus.read_byte(0x40)
+    cTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
+    fTemp = cTemp * 1.8 + 32
+    return fTemp
+
 #----------------------------------------------------------------------
 def create_rotating_log(path):
     """
@@ -44,17 +63,18 @@ def create_rotating_log(path):
     logger.addHandler(handler)
  
     while True:
-        result = instance.read()
-        if result.is_valid():
+        try:
             tm_stmp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            temp = 32 + (9 / 5) * result.temperature
-            humidity = result.humidity
-	    lux = lux_reading()
-	    dict_str = {'Time':tm_stmp, 'Humidity':humidity, 'Temperature':temp, 'Lumens':lux}
-        dict_str = str(dict_str)
+            humidity = get_humid()
+            time.sleep(0.3)
+            temp = get_temp()
+	        #lux = lux_reading()
+	        dict_str = {'Time':tm_stmp, 'Humidity':humidity, 'Temperature':temp } #, 'Lumens':lux}
+            dict_str = str(dict_str)
+            print dict_str
 	    logger.info(dict_str)
 	    time.sleep(3600)
-        else:
+        except:
             pass
  
 #----------------------------------------------------------------------
